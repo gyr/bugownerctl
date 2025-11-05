fe9fb57904c977fee8a37ed9c4a59fc177e5bd25
Gustavo Yokoyama Ribeiro <gyribeiro@suse.com>
2025-11-05 13:47:44 -0300

    Initial commit
    Add shell script to check if package has bugowner or it is whitelisted
    
    Signed-off-by: Gustavo Yokoyama Ribeiro <gyribeiro@suse.com>

---
+++ check_package_maintainer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+FILE="_maintainership.json"
+WHITELIST_FILE="whitelist_maintainership.json"
+PACKAGE=$1
+
+if jq -e '."'"$PACKAGE"'"' "$FILE" > /dev/null; then
+  echo "Package '$PACKAGE' is present in $FILE."
+else
+    echo "Package '$PACKAGE' is NOT present in $FILE."
+    if jq -e '.[] | select(. == "'"$PACKAGE"'")' "$WHITELIST_FILE" > /dev/null; then
+        echo "Package '$PACKAGE' is present in the $WHITELIST_FILE."
+    else
+        echo "Package '$PACKAGE' is NOT present in the $WHITELIST_FILE."
+    fi
+fi
